f2d0b3c11dc9dd40fc68b0ad2d066d5a4e45846b
fandywang <ofandywang@gmail.com>
2013-03-29 00:25:17 +0800

    add model_evaluator

---
+++ training/model_evaluator.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+# Copyright(c) 2013 python-sparselda project.
+# Author: Lifeng Wang (ofandywang@gmail.com)
+
+import logging
+import math
+import os
+import random
+import sys
+
+sys.path.append('..')
+from common.document import Document
+from common.model import Model
+from common.vocabulary import Vocabulary
+
+class ModelEvaluator(object):
+    """ModelQuality implements the evaluation method of lda model's quality.
+    """
+
+    def __init__(self, model, vocabulary):
+        self.model = model
+        self.vocabulary = vocabulary
+
+        # cache matrix p(w|z), indexed by word.
+        self.word_topic_dist = \
+                self.model.get_word_topic_dist(self.vocabulary.size())
+
+    def calculate_loglikelihood(self, documents):
+        """Returns the loglikelihood of documents.
+        p(D|M) = p(d1)p(d2)...
+        p(d) = p(w1)p(w2)...
+             = sum_z {p(z|d)p(w1|z)} * sum_z {p(z|d)p(w2|z)} * ...
+        log(p(d)) = log(sum_z p(z|d)p(w1|z)) + log(sum_z p(z|d)p(w2|z)) + ...
+        log(p(D|M)) = log(p(d1)) + log(p(d2)) + ...
+        """
+        loglikelihood = 0.0
+        for document in documents:
+            doc_dense_topic_dist = self._calculate_doc_topic_distribution(document)
+            doc_loglikelihood = 0.0
+            for word in document.document_pb.words:
+                word_topic_dist = self.word_topic_dist[word.id]
+                for topic, prob in enumerate(word_topic_dist):
+                    doc_loglikelihood += \
+                            math.log(prob * doc_dense_topic_dist[topic])
+            loglikelihood += doc_loglikelihood
+        return loglikelihood
+
+    def _calculate_doc_topic_distribution(self, document):
+        topic_hist_sum = 0
+        for non_zero in document.doc_topic_hist.sparse_topic_hist.non_zeros:
+            topic_hist_sum += non_zero.count
+
+        dense_topic_dist = []
+        for i in xrange(self.model.num_topics):
+            dense_topic_dist.append( \
+                    self.model.hyper_params.topic_prior * \
+                    document.get_topic_count(i) / \
+                    self.model.hyper_params.topic_prior * self.model.num_topics + \
+                    topic_hist_sum)
+        return dense_topic_dist
